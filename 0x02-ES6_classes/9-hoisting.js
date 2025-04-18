e03dd62379051fb4baf8234c2e73f6f80ba5dc30
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2025-04-18 04:11:08 +0200

    Refactor ALXClass: move class definition to 9-hoisting.js and update export format

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,5 +1,21 @@
-import ALXClass from './ALXClass';
+/* eslint-disable no-underscore-dangle */
+
 import StudentALX from './StudentALX';
+
+export default class ALXClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
 
 const class2019 = new ALXClass(2019, 'San Francisco');
 const class2020 = new ALXClass(2020, 'San Francisco');
@@ -10,4 +26,4 @@ const student3 = new StudentALX('Albert', 'Clinton', class2019);
 const student4 = new StudentALX('Donald', 'Bush', class2019);
 const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
-export default [student1, student2, student3, student4, student5];
+export const listOfStudents = [student1, student2, student3, student4, student5];
