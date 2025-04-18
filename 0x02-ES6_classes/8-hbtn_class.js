d160a1e55fa8788b162c7b01ff996c888b12a2a2
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2025-04-18 03:44:34 +0200

    Add ALXClass with constructor for size and location, and Symbol.toPrimitive method

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+export default class ALXClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return null;
+  }
+}
