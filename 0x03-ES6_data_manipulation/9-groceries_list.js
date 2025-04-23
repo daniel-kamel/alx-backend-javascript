348281f46e5d6ec94e736bbd6d3d571a348e5c6b
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2025-04-23 17:20:40 +0200

    Refactor groceriesList function to use Map for grocery items

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,16 +1,9 @@
 export default function groceriesList() {
-  const groceries = {
-    Apples: 10,
-    Tomatoes: 10,
-    Pasta: 1,
-    Rice: 1,
-    Banana: 5,
-  };
-
-  const groceryList = Object.entries(groceries).map(([item, quantity]) => ({
-    item,
-    quantity,
-  }));
-
-  return groceryList;
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+  return groceries;
 }
